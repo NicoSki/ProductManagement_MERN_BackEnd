3e4d983284cbdb4dd3b8f7cda061884cc5e76770
NICOLAS SKIBICKI <85801202+NicoSki@users.noreply.github.com>
2022-12-27 22:06:03 -0300

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connection.on('open', () => console.log('Success Connection to mongoBD'));
 
 async function connectDB({ host, port, dbName }) {
-    const uri = `mongodb://${host}:${port}/${dbName}`
+    const uri = "mongodb://mongo:kkpEJDnHRC8xvsRngXf9@containers-us-west-44.railway.app:7985"
     await mongoose.connect(uri, { useNewUrlParser: true })
 }
 
