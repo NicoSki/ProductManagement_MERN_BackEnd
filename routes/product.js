26b4c5353cfeb8d7a14b434d3d3520ed5c1dc685
NICOLAS SKIBICKI <85801202+NicoSki@users.noreply.github.com>
2023-01-02 19:58:44 -0300

    Update product.js

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const upload = require('../libs/storage');
 const productRouter = express.Router();
 
 const { addProduct, getProducts, deleteProduct, productCategory } = require('../controllers/productController');
 
 productRouter.get('/products',getProducts);
 productRouter.get('/products/:category',productCategory);
-productRouter.post('/products',upload.single('imgUrl'),addProduct);
+productRouter.post('/products',addProduct);
 productRouter.delete('/deleteProduct/:id',deleteProduct);
 
 
